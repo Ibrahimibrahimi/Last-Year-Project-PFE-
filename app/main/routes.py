acafbe5078b9e23756d407367925d55043909153
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-03-10 17:58:13 +0000

    handle /home route

--- app/main/routes.py
+++ app/main/routes.py
@@ -3,16 +3,7 @@ from flask_login import login_required
 from flask import render_template
 
 
-
-@main_bp.route("/")
+@main_bp.route("/home")
 @login_required
 def home():
-
-    return "HOME"
-
-@main_bp.route("/users")
-def allUsers():
-    # get all users
-    from app.models import User
-    Users = User.query.all()
-    return render_template("users.html",users=Users,l=True if len(Users) != 0 else False)
+    return render_template("home.html")
