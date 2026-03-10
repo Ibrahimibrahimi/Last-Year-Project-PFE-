669ec0b28dff5f98b77351c9bc36985fb49efc34
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-03-10 18:36:14 +0000

    editing...

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,4 +6,24 @@ from flask import render_template
 @main_bp.route("/home")
 @login_required
 def home():
+    
     return render_template("home.html")
+
+@main_bp.route("/courses")
+@login_required
+def courses():
+    return render_template("courses.html")
+
+@main_bp.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
+
+@main_bp.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@main_bp.route("/course/dd")
+def course_detail():
+    return render_template("course.html",)
