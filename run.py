841cde62d077b0fd527d224ed06accef1fcc0f9b
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-03-12 22:31:54 +0000

    convert createApp to create_app

--- run.py
+++ run.py
@@ -1,13 +1,12 @@
-from app import createApp
 from app.extensions import db
-
+from app import create_app
 
 # start the app only from this file
-if __name__ == "__main__" :
-    app = createApp()
-    with app.app_context() :
+if __name__ == "__main__":
+    app = create_app()
+    with app.app_context():
         db.create_all()
-    app.logger.info("SERVER STARTED SUCCESSFULLY")    
+    app.logger.info("SERVER STARTED SUCCESSFULLY")
     app.run(
         debug=True,
         host="0.0.0.0",
